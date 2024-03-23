aaa827492ec59d14bdf1f4c12bd60e90454f5e3c
Tanish2207 <bhamare.tn@gmail.com>
2024-03-23 10:22:26 +0530

    Init commit of branch

---
+++ client/app/doc-drafting/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DocDrafting = () => {
+  return (
+    <div>
+        <h1 className='text-center text-3xl font-semibold'>Document Drafting</h1>
+    </div>
+  )
+}
+
+export default DocDrafting
