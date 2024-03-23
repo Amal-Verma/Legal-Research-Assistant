1579738d2d5d657632a54608ec1be7a9399b92c0
AmalVerma <abverma_b23@it.vjti.ac.in>
2024-03-23 06:28:55 +0530

    Integrate Bot

--- client/app/integration/bot.js
+++ client/app/integration/bot.js
@@ -1,17 +1,21 @@
 import axios from "axios";
+import { useState } from "react";
 
 const getBot = () => {
   const [response, setResponse] = useState({});
   
   const getResponse = async (prompt) => {
     try{
-      let response = await axios.post("http://localhost:5000/bot/getResponse", {prompt});
-      setResponse(response.data.response);
+      let nresponse = await axios.post("http://localhost:5000/bot", {prompt});
+      setResponse(nresponse.data);
+      console.log(nresponse.data);
     }
-    catch{
-      console.log(error);
+    catch(e){
+      console.log(e);
     }
   }
 
   return {response, getResponse};
 }
+
+export default getBot;  
--- pythonServer/app.py
+++ pythonServer/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from bot import ai_chat_bot
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/bot', methods=['POST'])
 def scrape_endpoint():
